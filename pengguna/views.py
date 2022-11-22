833006c4ec719f6cbb64ae70bb4e1a972fca22a7
breadworshipper <azhrayashna@gmail.com>
2022-11-22 08:04:45 +0700

    Initial commit

--- pengguna/views.py
+++ pengguna/views.py
@@ -6,7 +6,7 @@ from django import forms
 from django.template import RequestContext
 from django.contrib.auth import authenticate
 
-from sirest_b09.models import Admin, Pelanggan, Restoran, Kurir
+from sirest_b09.models import *
 from .forms import AdminForm, PelangganForm, RestoranForm, KurirForm
 from json import dumps
 
@@ -95,3 +95,13 @@ def regis_kurir(request):
             'registered': registered})
 
 
+def show_kategori_makanan(request):
+      context = {
+            'kategori' : KategoriMakanan.objects.all().values(),
+      }
+      return render(request, 'kategori_makanan.html', context)
+
+def delete_kategori_makanan(request, kategori_id):
+      kategori_makanan = KategoriMakanan.objects.get(id = kategori_id)
+      kategori_makanan.delete()
+      return redirect('show_kategori_makanan')
--- sirest_b09/models.py
+++ sirest_b09/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class KategoriRestoran(models.Model):
+      id = models.IntegerField(primary_key=True)
+      name = models.CharField(max_length=50)
+
+class KategoriMakanan(models.Model):
+      id = models.IntegerField(primary_key=True)
+      name = models.CharField(max_length=50)
+
 class Admin(models.Model):
       email = models.EmailField()
       password = models.CharField(max_length=30)
@@ -38,7 +46,7 @@ class Restoran(models.Model):
       districts = models.CharField(max_length=50)
       city = models.CharField(max_length=50)
       province = models.CharField(max_length=50)
-      # category = models.ForeignKey(Kategori, on_delete=models.SET_NULL, blank=True, null=True) # ambil data dari kategori rstoran(name)
+      category = models.ForeignKey(KategoriRestoran, null=True, on_delete=models.SET_NULL)
       def __str__(self) -> str:
             return f"{self.name} | {self.email}"
 
@@ -57,3 +65,12 @@ class Kurir(models.Model):
       trans_type = models.CharField(choices=TRANS, max_length=10, blank=True, null=True)
       def __str__(self) -> str:
             return f"{self.name} | {self.email}"
+
+class Makanan(models.Model):
+      rname = models.ForeignKey(Restoran, on_delete=models.CASCADE)
+      foodname = models.CharField(max_length=50)
+      description = models.TextField()
+      stock = models.IntegerField()
+      price = models.FloatField()
+      kategori = models.ForeignKey(KategoriMakanan, blank=True, null=True, on_delete=models.SET_NULL)
+
