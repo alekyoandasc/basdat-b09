a08f981b494cfc85392c3cf0c1d77a93a114b460
Alek Yoanda <alexyoandatampu1@gmail.com>
2022-12-13 23:28:55 +0700

    Working at daftar restoran and detail restoran

--- restoran/urls.py
+++ restoran/urls.py
@@ -5,8 +5,8 @@ app_name="restoran"
 
 urlpatterns = [
     path('', read_daftar_restoran, name="read_daftar_restoran"),
-    path('<slug:rname>/<slug:rbranch>/detail/', read_detail_restoran, name="read_detail_restoran"),
-    path('<slug:rname>/<slug:rbranch>/menu/', read_menu_restoran, name="read_menu_restoran"),
+    path('detail/', read_detail_restoran, name="read_detail_restoran"),
+    path('menu/', read_menu_restoran, name="read_menu_restoran"),
     path('<slug:rname>/<slug:rbranch>/tambah-makanan/', create_makanan, name="create_makanan"),
     path('<slug:rname>/<slug:rbranch>/<slug:fname>/update-makanan/', update_makanan, name="update_makanan"),
     path('<slug:rname>/<slug:rbranch>/<slug:fname>/delete-makanan/', delete_makanan, name="delete_makanan"),
--- restoran/views.py
+++ restoran/views.py
@@ -4,24 +4,66 @@ from django.db import connection, transaction
 # Create your views here.
 def read_daftar_restoran(request):
     # TODO: Filter semua restoran
-    with connection.cursor() as cursor:
-        cursor.execute(
-            """SELECT RName FROM SIREST.RESTAURANT"""
-        )
-        fields = [field_name[0] for field_name in cursor.description]
-        rows = cursor.fetchall()
-        restaurants = [dict(zip(fields, row)) for row in rows]
-        print(restaurants)
-        context = {
-            "restaurants": restaurants,
-        }
-    return render(request, "restoran/read_daftar_restoran.html", context)
+    if request.COOKIES['user_type'] != 'restaurant':
+        with connection.cursor() as cursor:
+            cursor.execute(
+                """SELECT RName, RBranch, Rating FROM SIREST.RESTAURANT"""
+            )
+            fields = [field_name[0] for field_name in cursor.description]
+            rows = cursor.fetchall()
+            restaurants = [dict(zip(fields, row)) for row in rows]
+            context = {
+                "restaurants": restaurants,
+            }
+        return render(request, "restoran/read_daftar_restoran.html", context)
 
-def read_detail_restoran(request, rname, rbranch):
+def read_detail_restoran(request):
     # TODO: Filter restoran
-    return render(request, "restoran/read_detail_restoran.html")
+    if request.COOKIES['user_type'] != 'restaurant':
+        rname_get = request.GET.get("rname")
+        rbranch_get = request.GET.get("rbranch")
+        with connection.cursor() as cursor:
+            cursor.execute(
+                f"""
+                SELECT DISTINCT * FROM SIREST.RESTAURANT R
+                JOIN SIREST.RESTAURANT_CATEGORY RC
+                ON R.RCategory = RC.Id
+                WHERE R.RName='{rname_get}' AND R.RBranch='{rbranch_get}'
+                """
+            )
+            fields = [field_name[0] for field_name in cursor.description]
+            rows = cursor.fetchall()
+            restaurant = [dict(zip(fields, row)) for row in rows][0]
+            cursor.execute(
+                f"""
+                SELECT DISTINCT Day, StartHours, EndHours FROM SIREST.RESTAURANT_OPERATING_HOURS
+                WHERE Name='{rname_get}' AND Branch='{rbranch_get}'
+                """
+            )
+            fields = [field_name[0] for field_name in cursor.description]
+            rows = cursor.fetchall()
+            restaurant_oh = [dict(zip(fields, row)) for row in rows]
 
-def read_menu_restoran(request, rname, rbranch):
+            restaurant["operating_hours"] = restaurant_oh
+
+            cursor.execute(
+                f"""
+                SELECT DISTINCT PromoName FROM SIREST.RESTAURANT_PROMO
+                JOIN SIREST.Promo ON PId=Id
+                WHERE RName='{rname_get}' AND RBranch='{rbranch_get}'
+                """
+            )
+            fields = [field_name[0] for field_name in cursor.description]
+            rows = cursor.fetchall()
+            promos = [dict(zip(fields, row)) for row in rows]
+
+            restaurant["promos"] = promos
+            context = {
+                "restaurant": restaurant,
+            }
+        return render(request, "restoran/read_detail_restoran.html", context)
+
+def read_menu_restoran(request):
     # TODO: Filter restoran
     return render(request, "restoran/read_menu_restoran.html")
 
